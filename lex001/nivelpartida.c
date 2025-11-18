95b0922dfe15228df67e4d2079bc6c28c5c2d790
Carlos Dias <carlos.dias.o.n@gmail.com>
2025-11-18 06:01:25 -0300

    Atividade

---
+++ lex001/nivelpartida.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main()
+{
+    float capEstadio, numTorcedores, porcentagem;
+
+    printf("Qual a capacidade do estadio?\n");
+    scanf("%f", &capEstadio);
+    printf("Quantos torcedores apareceram no estadio?\n");
+    scanf("%f", &numTorcedores);
+
+    porcentagem = capEstadio / numTorcedores;
+
+    if (porcentagem > 0.9)
+    {
+        printf("LOTADO!\n");
+    }
+    else if (porcentagem >= 0.7)
+    {
+        printf("Otima presenca de publico!\n");
+    }
+    else if (porcentagem >= 0.5)
+    {
+        printf("Publico razoavel!\n");
+    }
+    else
+    {
+        printf("Morumbis!\n");
+    }
+}
